8765068988332b85adff5372740422e83d0a4342
Luke Frahm <lukefrahm@gmail.com>
2018-09-26 13:33:30 -0500

    Add files via upload

---
+++ WakeHost/Host.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Net;
+
+namespace WakeHost
+{
+    [XmlRoot("Host", IsNullable = false)]
+    internal sealed class Host
+    {
+        [XmlAttribute]
+        internal string Name { get; set; }
+        internal string FQDN { get; set; }
+        internal byte[] IpAddress
+        {
+            get
+            {
+                return IpAddress;
+            }
+            set
+            {
+                IpAddress = IPAddress.Parse(value.ToString()).GetAddressBytes();
+            }
+        }
+        [XmlArray]
+        internal Interface[] MacAddresses { get; set; }
+        
+        internal Host()
+        {
+
+        }
+    }
+
+    internal class Interface
+    {
+        [XmlAttribute]
+        internal byte Position { get; set; }
+        internal byte[] MacAddress
+        {
+            get { return MacAddress; }
+            set
+            { 
+                MacAddress = value.Length == 6 ? value : throw new ArgumentException("Invalid MAC address.");
+            }
+        }
+    }
+}
